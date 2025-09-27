22eea86a13295e0fc387e4181137de5113ae1502
FireStreaker2 <michaelccc2019@outlook.com>
2025-09-27 16:42:42 -0700

    mildly congested karaoke songs

--- util/config.py
+++ util/config.py
@@ -10,4 +10,5 @@ music = "./music"
 karaokes = [
     "https://www.youtube.com/watch?v=H_Nc-zjRmK4",
     "https://www.youtube.com/watch?v=h3jBOcBn_tU",
+    "https://www.youtube.com/watch?v=rXmJ61_Axbo",
 ]
